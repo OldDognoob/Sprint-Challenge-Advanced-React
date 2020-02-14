16b1773bc0ef0a8a6576af6912cc647deb42974a
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-14 12:32:05 +0200

    create on test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+// import { render } from '@testing-library/react';
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+
+describe('App', () => {
+  it('renders an element with specific text', () => {
+    // 1- setup, render app to the virtual dom
+    const wrapper = rtl.render(<App />);
+    // 2- capture the element of interest
+    const element = wrapper.queryByText(/List of Players/i);
+    // wrapper.debug()
+    // 3- assert thing in DOM
+    expect(element).toBeInTheDocument();
+  })
+
+})
